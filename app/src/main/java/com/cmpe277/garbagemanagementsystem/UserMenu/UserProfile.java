574f60fc15820acc2d06a23921e31afe53d052af
darshcloud <darshini.vn@gmail.com>
2023-04-22 01:04:50 -0700

    View User Profile Functionality changes

--- app/src/main/java/com/cmpe277/garbagemanagementsystem/UserMenu/UserProfile.java
+++ app/src/main/java/com/cmpe277/garbagemanagementsystem/UserMenu/UserProfile.java
@@ -1,16 +1,52 @@
 package com.cmpe277.garbagemanagementsystem.UserMenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.cmpe277.garbagemanagementsystem.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserProfile extends AppCompatActivity {
+
+    private DatabaseReference mDatabaseRef;
+    private TextView mNameTextView, mEmailTextView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        mNameTextView = findViewById(R.id.name);
+        mEmailTextView = findViewById(R.id.email);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        String user_id = mAuth.getCurrentUser().getUid();
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("User").child(user_id);
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child("userName").getValue(String.class);
+                String email = snapshot.child("userEmail").getValue(String.class);
+
+                mNameTextView.setText(name);
+                mEmailTextView.setText(email);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
